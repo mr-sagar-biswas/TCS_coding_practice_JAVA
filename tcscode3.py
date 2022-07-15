a3d6f80b0e7d9c8780f8728d140ed44910dfc48e
Sagar <sagarbiswasbunny@gmail.com>
2022-07-16 00:37:15 +0530

    deleted some files

--- tcscode3.py
+++
@@ -1,9 +0,0 @@
-n=input()
-evensum=0
-oddsum=0
-for i in range(len(n)):
-    if i%2==0:
-        evensum=evensum+int(n[i])
-    if i%2==1:
-        oddsum=oddsum+int(n[i])
-print(abs(evensum-oddsum))
--- tcscode5and6.py
+++
@@ -1,5 +0,0 @@
-n =int(input())
-if(n%2==0):
-    print(3**(n//2-1))
-if(n%2==1):
-    print(2**(n//2))
